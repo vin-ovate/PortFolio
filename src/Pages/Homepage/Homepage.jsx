49e48240b6556fa9315794e872ac36406864b54f
vin-ovate <vinaypower69@gmail.com>
2024-12-26 15:20:52 +0530

    Update Homepage.jsx

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -11,7 +11,7 @@ const Homepage = () => {
       <div className="homepage-showcase">
         <img className="homepage1-logo" src="./assets/vin.png" alt="Logo" />
         <div className="homepage-title">
-          <h1 className="homepage-main-title">Inno-Vin.tech</h1>
+          <h1 className="homepage-main-title">Vinay's Portfolio</h1>
           <h2 className="homepage-sub-title">Quality edits - Affordable prices!</h2>
           <p className="homepage-lead homepage-hide-on-small">
             Welcome to your one-stop shop for edits! We're thrilled to offer you
